4759a1b72fad637781a1f658e56c8fe45dd4b999
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-31 22:41:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const arrayOfNames = ["Mai", "Zach", "Eric"]
+const eventName = "surprise"
+
+function writeCards(arrayOfNames, event){
+    let arr2 = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        arr2.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arr2;
+}
+
+
+
+function countDown(num) {
+    //let i = num
+    while (num >= 0){
+        console.log(num--)
+    }
+}
